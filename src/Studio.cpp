4f23abdcb828031b2b64e31330a93b235e345dec
talpar <elhadan@post.bgu.ac.il>
2021-11-06 18:49:30 +0200

    studio and workout wip

--- src/Studio.cpp
+++ src/Studio.cpp
@@ -6,27 +6,64 @@ using namespace std;
 
 
 
+
 Studio::Studio(const std::string &configFilePath) {
     //here we define the list of trainers and list of workouts plains
     fstream newfile;
     newfile.open(configFilePath,ios::in);
     if (newfile.is_open()){   //checking whether the file is open
         string tp;
+        //parse the first line - number of trainers
         getline(newfile, tp);
         int NumOfTrainers;
         sscanf(tp.data(), "%d", &NumOfTrainers);
+//        cout << NumOfTrainers << endl;
+        //parse the second line - trainers capacity
         getline(newfile, tp); //string of the trainers (6,6,4,5) for example
-        cout << NumOfTrainers << endl;
         int n = tp.length();
-        char Trainers_Capasitys[n+1];
-        strcpy(Trainers_Capasitys, tp.c_str()); //here we change the trainers capasity string into char array
+        char Trainers_Capacitys[n+1];
+        strcpy(Trainers_Capacitys, tp.c_str()); //here we change the trainers capacity string into char array
         for(int i = 0; i <n ; i++){
-          trainers.push_back(new Trainer(Trainers_Capasitys[i] - '0')); //convert char to int and insert to trainer vectos
-            cout << trainers[i]->getCapacity() << endl;
+            int char_to_num = (Trainers_Capacitys[i] - '0');
+            if(char_to_num != -4) {
+                trainers.push_back(new Trainer(char_to_num)); //convert char to int and insert to trainer vectos
+            }
         }
 
-        //while(getline(newfile, tp)){ //read data from file object and put it into string.
+        //-------------check for later------------
+//        for(int i = 0; i<=NumOfTrainers; i++) {
+//                            cout << trainers[i]->getCapacity() << endl;
+//        }
+        //parse the last line - Workouts
+        int id = 0;
+        while(getline(newfile, tp)){
+            vector<string> curr_row;
+            stringstream s_stream(tp);
+            while(s_stream.good()) {
+                string substr;
+                getline(s_stream, substr, ',');
+                curr_row.push_back(substr);
+            }
+            string name = curr_row[0];
+            string stype = curr_row[1];
+            string sprice = curr_row[2];
+            WorkoutType type;
+            if(stype == "anerobic"){
+                type = ANAEROBIC;
+            }
+            if(stype == "mixed"){
+                type = MIXED;
+            }
+            if(stype == "cardio"){
+                type = CARDIO;
+            }
+            int price = stoi(sprice);
+            cout <<substr<< endl;
+//            Workout test = new Workout(id,name,price,type);
+//            workout_options.push_back(new Workout(id,name,price,type));
+            id++;
 
+        }
     }
     newfile.close(); //close the file object.
 }
--- src/Trainer.cpp
+++ src/Trainer.cpp
@@ -4,3 +4,7 @@
 #include "../include/Trainer.h"
 
 Trainer::Trainer(int t_capacity):capacity(t_capacity){}
+
+int Trainer::getCapacity() const{
+    return capacity;
+}
