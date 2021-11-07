a583555fd69434566643dde9276ed552b9453cf3
talpar <elhadan@post.bgu.ac.il>
2021-11-07 19:49:44 +0200

    refactored studio, finished workout.cpp

--- src/Studio.cpp
+++ src/Studio.cpp
@@ -15,68 +15,100 @@ Studio::Studio(const std::string &configFilePath) {
     if (newfile.is_open()){   //checking whether the file is open
         string tp;
         //parse the first line - number of trainers
-        getline(newfile, tp);
-        int NumOfTrainers;
-        sscanf(tp.data(), "%d", &NumOfTrainers);
-        //parse the second line - trainers capacity
-        getline(newfile, tp); //string of the trainers (6,6,4,5) for example
-        int n = tp.length();
-        char Trainers_Capacitys[n+1];
-        strcpy(Trainers_Capacitys, tp.c_str()); //here we change the trainers capacity string into char array
-        for(int i = 0; i <n ; i++){
-            int char_to_num = (Trainers_Capacitys[i] - '0');
-            if(char_to_num != -4) {
-                trainers.push_back(new Trainer(char_to_num)); //convert char to int and insert to trainer vectos
+        getline(newfile, tp); // first row is "#trainers" dont need it
+        while(!tp.empty()){
+            //#trainers
+            int NumOfTrainers;
+            if(tp == "# Number of trainers"){
+                getline(newfile, tp); // We get now the data of number of trainers
+                sscanf(tp.data(), "%d", &NumOfTrainers); // string to int, because we know what is the second line
+                getline(newfile, tp); // We finished with this line and go to the next one
+                cout << NumOfTrainers << endl;
             }
-        }
+            //#capacity
 
-        //-------------check for later------------
-//        for(int i = 0; i<=NumOfTrainers; i++) {
-//                            cout << trainers[i]->getCapacity() << endl;
-//        }
-        //parse the last line - Workouts
-        int id = 0;
-        while(getline(newfile, tp) && !tp.empty()){
-            vector<string> curr_row;
-            stringstream s_stream(tp);
-            while(s_stream.good()) {
-                string substr;
-                getline(s_stream, substr, ',');
-                curr_row.push_back(substr);
+            if(tp == "# Traines") {
+//                cout << tp << endl;
+                getline(newfile, tp); //We get now the data of trainers capacity
+                int n = tp.length();
+                char Trainers_Capacitys[n+1];
+                strcpy(Trainers_Capacitys, tp.c_str()); //here we change the trainers capacity string into char array
+                for(int i = 0; i <n ; i++){
+                    int char_to_num = (Trainers_Capacitys[i] - '0');
+                    if(char_to_num != -4) {
+                        trainers.push_back(new Trainer(char_to_num)); //convert char to int and insert to trainer vectos
+                    }
+                }
+                for (int i = 0; i < trainers.size(); ++i) {
+                    cout << trainers[i]->getCapacity() << endl;
+
+                }
+                getline(newfile, tp); // We finished with this line and go to the next one
             }
-            string name;
-            string stype;
-            string sprice;
-            for (int i = 0; i < curr_row.size(); i++) {
-                switch(i){
-                    case 0:
-                        name = curr_row[i];
-                        break;
-                    case 1:
-                        stype = curr_row[i];
-                        break;
-                    case 2:
-                        sprice = curr_row[i];
-                        break;
+            //#workout
+            if(tp == "# Work Options") {
+//                cout << tp.length() << endl;
+                int Workout_id = 0;
+                while(getline(newfile, tp) && !tp.empty()){
+                    vector<string> curr_row;
+                    stringstream s_stream(tp);
+                    while(s_stream.good()) {
+                        string substr;
+                        getline(s_stream, substr, ',');
+                        curr_row.push_back(substr);
+                    }
+                    string name;
+                    string stype;
+                    string sprice;
+                    for (int i = 0; i < curr_row.size(); i++) {
+                        switch(i){
+                            case 0:
+                                name = curr_row[i];
+                                break;
+                            case 1:
+                                stype = curr_row[i];
+                                break;
+                            case 2:
+                                sprice = curr_row[i];
+                                break;
+                        }
+                    }
+                    curr_row.empty();
+                    WorkoutType type;
+                    if(stype == "Anerobic"){
+                        type = WorkoutType::ANAEROBIC;
+                    }
+                    if(stype == "Mixed"){
+                        type = WorkoutType::MIXED;
+                    }
+                    if(stype == "Cardio"){
+                        type = WorkoutType::CARDIO;
+                    }
+                    int price = stoi(sprice);
+                    workout_options.push_back(Workout(Workout_id,name,price,type));
+                    Workout_id++;
+                }
+
+                for (int i = 0; i < workout_options.size(); ++i) {
+                    cout << workout_options[i].getName() << endl;
+
                 }
             }
-            curr_row.empty();
-            WorkoutType type;
-            if(stype == "Anerobic"){
-                type = WorkoutType::ANAEROBIC;
+            //blank line skip
+            getline(newfile, tp);
+
+            //exit file when done, AKA we finished with workout types
+            if(workout_options.size() > 0) {
+                newfile.close(); //close the file object.
+                break;
             }
-            if(stype == "Mixed"){
-                type = WorkoutType::MIXED;
-            }
-            if(stype == "Cardio"){
-                type = WorkoutType::CARDIO;
-            }
-            int price = stoi(sprice);
-            workout_options.push_back(Workout(id,name,price,type));
-            id++;
+
         }
+
     }
-    newfile.close(); //close the file object.
+
+
+
 }
 
 void Studio::start() {
