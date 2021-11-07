47567f27014e2ea8604ca7396fc8953581cc5550
daniel-ubuntu <elhadan@post.bgu.ac.il>
2021-11-08 01:32:38 +0200

    added clion makefile, we can debuggit add .git add .

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/talpar/CLionProjects/Ass1
+# Install script for directory: /home/daniel/CLionProjects/Ass1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/talpar/CLionProjects/Ass1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/daniel/CLionProjects/Ass1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
